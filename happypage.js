b8d8856fb108d9a30276dd50c1bf1f554f3567a2
Piyush <piyushsoni.210@gmail.com>
2020-08-10 13:50:01 +0530

    fixed the upload issue

---
+++ happypage.js
@@ -0,0 +1,190 @@
+import React, { Component } from 'react'
+import '../App.css'
+import './happypage.css'
+import happyimage1 from '../images/happyimage1.jpg'
+import happyimage2 from '../images/happyimage2.jpg'
+import happyimage3 from '../images/happyimage3.jpg'
+import happyimage4 from '../images/happyimage4.jpg'
+import happyimage5 from '../images/happyimage5.jpg'
+import happyimage6 from '../images/happyimage6.jpg'
+import happyimage7 from '../images/happyimage7.jpg'
+import happyimage8 from '../images/happyimage8.jpg'
+import happyimage9 from '../images/happyimage9.jpg'
+import happyimage10 from '../images/happyimage10.jpg'
+import happyimage11 from '../images/happyimage11.jpg'
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import backgroundimage from '../images/backgroundwebpage.jpg'
+import Spring from './springtut'
+AOS.init({
+    delay:400,
+duration:1000
+}
+);
+ class Happypage extends Component {
+    
+    render() {  
+
+        return (
+            <div className="body" styles={{ background: "" }}>
+                    <br></br><br></br>
+                <div className="sabkaBaap">
+                    <div className="top_content">
+                    <header className="main-header left1">
+                    <p><h2><span>Promise 2</span></h2>
+                    Love you much jaani and this make your day more special.
+                    I know we are not together and this hurts you but i will do my best to make this day awsome</p>
+                    </header>
+                    <header className="main-header right1">
+                  
+                    <p><h2><span>Promise 1</span></h2>Love you much jaani and this make your day more special.
+                    I know we are not together and this hurts you but i will do my best to make this day awsome</p>
+                    </header>
+                    </div>
+
+
+                    <div className="top_content">
+                    <header className="temp-header ">
+                    <h1>Happy<span> 1st Aniversary </span>Jaani</h1> 
+                    
+                    </header>
+                    <header className="main-header ">
+                    <h1>Happy<span> 1st Aniversary </span>Jaani</h1> 
+                    <p>Love you much jaani and this make your day more special.
+                    I know we are not together and this hurts you but i will do my best to make this day awsome</p>
+                    </header>
+                    <header className="temp-header ">
+                    <h1>Happy<span> 1st Aniversary </span>Jaani</h1> 
+                    
+                    </header>
+                    </div>
+
+
+                    <div className="top_content">
+                    <header className="main-header left2">
+                   
+                    <p><h2><span>Promise 4</span></h2>Love you much jaani and this make your day more special.
+                    I know we are not together and this hurts you but i will do my best to make this day awsome</p>
+                    </header>
+                    <header className="main-header right2">
+                    <p><h2><span>Promise 3</span></h2>Love you much jaani and this make your day more special.
+                    I know we are not together and this hurts you but i will do my best to make this day awsome</p>
+                    </header>
+                    </div>
+                    <div className="top_content">
+                    <header className="main-header left3">
+               
+                    <p><h2><span>Promise 6</span></h2>Love you much jaani and this make your day more special.
+                    I know we are not together and this hurts you but i will do my best to make this day awsome</p>
+                    </header>
+                    <header className="main-header right3">
+                    <p><h2><span>Promise 5</span></h2>Love you much jaani and this make your day more special.
+                    I know we are not together and this hurts you but i will do my best to make this day awsome</p>
+                    </header>
+                    </div>
+                <header className="main-header">
+                   
+                    <p><h2><span>Promise 7</span></h2>Love you much jaani and this make your day more special.
+                    I know we are not together and this hurts you but i will do my best to make this day awsome</p>
+                </header>
+                </div>
+                <main className="container">
+                    <br></br><br></br><br></br><br></br><br></br>
+                <section className="card" 
+                        data-aos="fade-left"
+                        data-aos-offset="200"
+                        data-aos-delay="50"
+                        data-aos-duration="1000"
+                        data-aos-easing="ease-in-out"
+                        data-aos-mirror="true"
+                ><img src={happyimage1} alt=""></img>
+                    <div>
+                        <h1>bla lala</h1>
+                        <p>Lorem, ipsum dolor sit amet consectetur 
+                            adipisicing elit. Esse rerum culpa repudiandae, 
+                            dicta sit dolore necessitatibus animi molestias et facere 
+                            sunt impedit? Distinctio omnis necessitatibus earum vitae, 
+                            accusantium eius consequuntur?
+                        </p>
+                    </div>
+                </section>
+                <section className="card" 
+                        data-aos="fade-right"
+                        data-aos-offset="200"
+                        data-aos-delay="50"
+                        data-aos-duration="1000"
+                        data-aos-easing="ease-in-out"
+                        data-aos-mirror="true"
+                >
+                    <img src={happyimage2} alt=""></img>
+                    <div>
+                        <h1>bla lala</h1>
+                        <p>Lorem, ipsum dolor sit amet consectetur 
+                            adipisicing elit. Esse rerum culpa repudiandae, 
+                            dicta sit dolore necessitatibus animi molestias et facere 
+                            sunt impedit? Distinctio omnis necessitatibus earum vitae, 
+                            accusantium eius consequuntur?</p>
+                    </div>
+                </section>
+                <section className="card" 
+                        data-aos="fade-left"
+                        data-aos-offset="200"
+                        data-aos-delay="50"
+                        data-aos-duration="1000"
+                        data-aos-easing="ease-in-out"
+                        data-aos-mirror="true"
+                >
+                    <img src={happyimage3} alt=""></img>
+                    <div>
+                        <h1>bla lala</h1>
+                        <p>Lorem, ipsum dolor sit amet consectetur 
+                            adipisicing elit. Esse rerum culpa repudiandae, 
+                            dicta sit dolore necessitatibus animi molestias et facere 
+                            sunt impedit? Distinctio omnis necessitatibus earum vitae, 
+                            accusantium eius consequuntur?</p>
+                    </div>
+                </section>
+                <section className="card" 
+                        data-aos="fade-right"
+                        data-aos-offset="200"
+                        data-aos-delay="50"
+                        data-aos-duration="1000"
+                        data-aos-easing="ease-in-out"
+                        data-aos-mirror="true"
+                >
+                    <img src={happyimage4} alt=""></img>
+                    <div>
+                        <h1>bla lala</h1>
+                        <p>Lorem, ipsum dolor sit amet consectetur 
+                            adipisicing elit. Esse rerum culpa repudiandae, 
+                            dicta sit dolore necessitatibus animi molestias et facere 
+                            sunt impedit? Distinctio omnis necessitatibus earum vitae, 
+                            accusantium eius consequuntur?</p>
+                    </div>
+                </section>
+                <section className="card" 
+                        data-aos="fade-left"
+                        data-aos-offset="200"
+                        data-aos-delay="50"
+                        data-aos-duration="1000"
+                        data-aos-easing="ease-in-out"
+                        data-aos-mirror="true"
+                >
+                    <img src={happyimage6} alt=""></img>
+                    <div>
+                        <h1>bla lala</h1>
+                        <p>Lorem, ipsum dolor sit amet consectetur 
+                            adipisicing elit. Esse rerum culpa repudiandae, 
+                            dicta sit dolore necessitatibus animi molestias et facere 
+                            sunt impedit? Distinctio omnis necessitatibus earum vitae, 
+                            accusantium eius consequuntur?</p>
+                    </div>
+                </section>
+            </main>
+            </div>
+        )
+    }
+}
+
+export default Happypage
